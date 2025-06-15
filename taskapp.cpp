8f15c6e68e1405b826d5112f04db8c8cc0dd58c9
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-06-15 16:53:30 +0530

    Update taskapp.cpp

--- taskapp.cpp
+++ taskapp.cpp
@@ -4,33 +4,68 @@
 #include <cstdlib>
 
 using namespace std;
-vector<string>liist={};
+vector<string> liist = {};
 string newNote;
-void addNote(){
-    
-    
-    cout<<"Add new Note->";
-    
-    
-    getline(cin,newNote);
-     
- 
-    
-    
+void addNote()
+{
 
+    cout << "Add new Note->";
+    
+    getline(cin, newNote);
+
+
+    
 }
-int main() {
-   while(true){
-    
-    addNote();
-    system("cls");
-    liist.push_back(newNote);
-    cout<<"---------------------Tasks"<<endl;
-    for(int i=0;i<liist.size();i++){
-        cout<< "Task"<<" "<<i<<"->"<<liist[i]<<endl;
+//refreshing the tasks 
+void refresh(vector<string>&refreshing){
+    for(int i=0;i<refreshing.size();i++){
+        
+                cout << "Task" << " " << i + 1 << "->" << refreshing[i] << endl;
+            
+           
     }
-    cout<<"---------------------"<<endl;
+     cout << "---------------------" << endl;
     
-   }
- 
+}
+int main()
+{
+    while (true)
+    {
+        
+        addNote();
+        //quit option 
+        if (newNote == ":q") {
+            system("cls");
+            cout << "---------------------Tasks" << endl;
+           refresh(liist);
+            
+            break;};
+       //delete option 
+        if (newNote == "-d")
+        {
+            int inp;
+            cout << " Token number of task to be deleted : " ;
+            cin >> inp;
+            cin.ignore();
+            vector<string>::iterator it = liist.begin();
+            liist.erase(it + inp-1);
+            
+            
+            system("cls");
+            cout << "---------------------Tasks" << endl;
+           refresh(liist);
+        
+        }
+        else
+        { 
+
+        
+            system("cls");
+
+            liist.push_back(newNote);
+            cout << "---------------------Tasks" << endl;
+           refresh(liist);
+        }
+    }
+    return 0;
 }
